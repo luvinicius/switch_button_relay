b600a898aed296f55314236de72a12628613b05c
luvinicius <lucas.v.pimenta@gmail.com>
2022-07-17 21:39:47 -0300

    Bug fix

--- switch_button_relay.ino
+++ switch_button_relay.ino
@@ -14,7 +14,7 @@ void setup() {
 
   pinMode(RELAY_1_PIN, OUTPUT); // set-up first relay pin and start it as turn-off
   digitalWrite(RELAY_1_PIN, HIGH);
-  
+
   pinMode(RELAY_2_PIN, OUTPUT); // set-up second relay pin and start it as turn-off
   digitalWrite(RELAY_2_PIN, HIGH);
 
@@ -29,26 +29,26 @@ void loop() {
   beforeState = buttonState; // store the current state of button before read the new state
   buttonState = digitalRead(BUTON_PIN); // read new current state of button
 
-  
+
 
   // If the button is released but before it was pressed, its mean one "click"
   if (buttonState == LOW) {
-    if(beforeState = HIGH){
+    if (beforeState == HIGH) {
       // each click in the button will selected the next relay
       selected_relay = selected_relay + 1;
       Serial.println("Button click event!");
     }
   }
-  if(selected_relay > NUMBER_OF_RELAYS){ 
+  if (selected_relay > NUMBER_OF_RELAYS) {
     // If its selected an relay out of bound its mean none relay is selected
     selected_relay = 0; // set selected relay to none
     // turn-off last relay
     digitalWrite(RELAY_2_PIN, HIGH);
     Serial.println("Turn-off second relay!");
-  }else if(selected_relay == 1){ // turn-on first relay
+  } else if (selected_relay == 1) { // turn-on first relay
     digitalWrite(RELAY_1_PIN, LOW);
     Serial.println("Turn-on first relay!");
-  }else if(selected_relay == 2){ // turn-off first relay nd turn-on second relay
+  } else if (selected_relay == 2) { // turn-off first relay nd turn-on second relay
     digitalWrite(RELAY_1_PIN, HIGH);
     Serial.println("Turn-off first relay!");
     digitalWrite(RELAY_2_PIN, LOW);
