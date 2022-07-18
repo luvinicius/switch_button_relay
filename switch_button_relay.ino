d5aecd78a71498086c81b1342f00bcb3c5d4332f
luvinicius <lucas.v.pimenta@gmail.com>
2022-07-17 22:25:41 -0300

    Code improvement

--- switch_button_relay.ino
+++ switch_button_relay.ino
@@ -29,29 +29,27 @@ void loop() {
   beforeState = buttonState; // store the current state of button before read the new state
   buttonState = digitalRead(BUTON_PIN); // read new current state of button
 
-
-
   // If the button is released but before it was pressed, its mean one "click"
   if (buttonState == LOW) {
     if (beforeState == HIGH) {
       // each click in the button will selected the next relay
       selected_relay = selected_relay + 1;
       Serial.println("Button click event!");
+      if (selected_relay > NUMBER_OF_RELAYS) {
+        // If its selected an relay out of bound its mean none relay is selected
+        selected_relay = 0; // set selected relay to none
+        // turn-off last relay
+        digitalWrite(RELAY_2_PIN, HIGH);
+        Serial.println("Turn-off second relay!");
+      } else if (selected_relay == 1) { // turn-on first relay
+        digitalWrite(RELAY_1_PIN, LOW);
+        Serial.println("Turn-on first relay!");
+      } else if (selected_relay == 2) { // turn-off first relay nd turn-on second relay
+        digitalWrite(RELAY_1_PIN, HIGH);
+        Serial.println("Turn-off first relay!");
+        digitalWrite(RELAY_2_PIN, LOW);
+        Serial.println("Turn-on second relay!");
+      }
     }
-  }
-  if (selected_relay > NUMBER_OF_RELAYS) {
-    // If its selected an relay out of bound its mean none relay is selected
-    selected_relay = 0; // set selected relay to none
-    // turn-off last relay
-    digitalWrite(RELAY_2_PIN, HIGH);
-    Serial.println("Turn-off second relay!");
-  } else if (selected_relay == 1) { // turn-on first relay
-    digitalWrite(RELAY_1_PIN, LOW);
-    Serial.println("Turn-on first relay!");
-  } else if (selected_relay == 2) { // turn-off first relay nd turn-on second relay
-    digitalWrite(RELAY_1_PIN, HIGH);
-    Serial.println("Turn-off first relay!");
-    digitalWrite(RELAY_2_PIN, LOW);
-    Serial.println("Turn-on second relay!");
   }
 }
